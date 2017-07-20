06ad532a4c2489949e04c816802fad6176650a41
Shubham Namdeo <shubham.n@consultadd.com>
2017-07-20 10:04:36 +0530

    first commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+===================
+django_project_name
+===================
+
+
+Add a short description here!
+
+
+Description
+===========
+
+A longer description of your project goes here...
+
---
+++ tests/test_skeleton.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from django_project_name.skeleton import fib
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
